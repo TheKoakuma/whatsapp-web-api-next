c96017872d9f4a73c8ce10188a284a555e5daf94
TheKoakuma <jose.junior943@ufpr.br>
2023-06-20 20:08:03 -0300

    Setando Socket.io

--- pages/api/connect.js
+++ pages/api/connect.js
@@ -1,4 +1,23 @@
 import { Server } from 'socket.io'
+import { Client, LocalAuth } from 'whatsapp-web.js'
+
+const client=new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
+})
 
 const SocketHandler = (req, res) => {
   if (res.socket.server.io) {
@@ -11,11 +30,40 @@ const SocketHandler = (req, res) => {
     });
     res.socket.server.io = io
 
-    io.on('connection', socket => {
-      socket.on('input-change', msg => {
-        socket.broadcast.emit('update-input', msg)
-      })
-    })
+    client.initialize()
+
+    io.on('connection', function(socket) {
+      socket.emit('message', 'Connecting...');
+    
+      client.on('qr', (qr) => {
+        console.log('QR RECEIVED', qr);
+        qrcode.toDataURL(qr, (err, url) => {
+          socket.emit('qr', url);
+          socket.emit('message', 'QR Code received, scan please!');
+        });
+      });
+    
+      client.on('ready', () => {
+        socket.emit('ready', 'Whatsapp is ready!');
+        socket.emit('message', 'Whatsapp is ready!');
+      });
+    
+      client.on('authenticated', () => {
+        socket.emit('authenticated', 'Whatsapp is authenticated!');
+        socket.emit('message', 'Whatsapp is authenticated!');
+        console.log('AUTHENTICATED');
+      });
+    
+      client.on('auth_failure', function(session) {
+        socket.emit('message', 'Auth failure, restarting...');
+      });
+    
+      client.on('disconnected', (reason) => {
+        socket.emit('message', 'Whatsapp is disconnected!');
+        client.destroy();
+        client.initialize();
+      });
+    });
   }
   res.end()
 }
--- pages/connect.js
+++ pages/connect.js
@@ -8,7 +8,7 @@ const Home = () => {
   useEffect(() => socketInitializer(), [])
 
   const socketInitializer = async () => {
-    await fetch('/api/socket');
+    await fetch('/api/connect');
     socket = io(undefined,{
         path:"/api/socket_io"
     })
@@ -20,6 +20,23 @@ const Home = () => {
     socket.on('update-input', msg => {
       setInput(msg)
     })
+
+    socket.on('message', function(msg) {
+      $('.logs').append($('<li>').text(msg));
+    });
+
+    socket.on('qr', function(src) {
+      $('#qrcode').attr('src', src);
+      $('#qrcode').show();
+    });
+
+    socket.on('ready', function(data) {
+      $('#qrcode').hide();
+    });
+
+    socket.on('authenticated', function(data) {
+      $('#qrcode').hide();
+    });
   }
 
   const onChangeHandler = (e) => {
@@ -28,11 +45,13 @@ const Home = () => {
   }
 
   return (
-    <input
-      placeholder="Type something"
-      value={input}
-      onChange={onChangeHandler}
-    />
+    <div>
+      <h1>Whatsapp API</h1>
+      <p>Powered by TheKoakuma</p>
+      <img src="" alt="QR Code" id="qrcode"></img>
+      <h3>Logs:</h3>
+      <ul class="logs"></ul>
+    </div>
   )
 }
 
